5fc1b07f5b5f4caacc18c051e14d46eb36c25097
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-11-24 13:07:00 -0300

    Create full_k41s.mk

---
+++ device/lge/k41s/full_k41s.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/lge/k41s/device.mk)
+
+PRODUCT_NAME := full_k41s
+PRODUCT_DEVICE := k41s
+PRODUCT_BRAND := LG
+PRODUCT_MODEL := LG K41S
+PRODUCT_MANUFACTURER := LGE
